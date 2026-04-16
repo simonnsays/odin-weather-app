c9766ad662150e73a644fce4a784ba710fdc5cda
Darel <johndarel0914@gmail.com>
2026-04-16 16:12:01 +0800

    Make app work in the console

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
-const locInput = 'dublin'
+const locInput = 'kaypian'
 const key = 'YAGVLE6WETGXYAW8PQMHBUJAM'
 const req = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locInput}?key=${key}`
-
+let isCelc = true 
 // let temp = '0°' // degree in celcius
 const getWeather = async () => {
     try {
@@ -20,16 +20,32 @@ const interpretData = async() => {
     const curr = data.currentConditions
     return {
         location: data.resolvedAddress,
-        temp: curr.temp,    // in farenheight
+        temp: curr.temp,            // in farenheight
+        feelslike: curr.feelslike,  // in farenheight
         icon: curr.icon,
-        condition: curr,
-        feelslike: curr.feelslike
+        condition: curr.conditions,
     }
 }
 
-// interpretData()
-console.log(await interpretData())
+const interpretTemp = (num) => {
+    
+    return isCelc 
+    ? Math.round((num - 32) * 5 / 9 * 10) / 10
+    : Math.round(num * 10) / 10
+}
 
+const displayWeather = async () => {
+    const data = await interpretData()
+
+    console.log(data.location)
+    console.log(`${interpretTemp(data.temp)}°`)
+    console.log(`Feels like ${interpretTemp(data.feelslike)}°`)
+    console.log(data.condition)
+}
+
+// interpretData()
+// console.log(await interpretData())
+displayWeather()
 
 // farenheight: Math.round(curr.temp * 10) / 10,
 // celcius: Math.round((curr.temp - 32) * 5 / 9 * 10) / 10,
