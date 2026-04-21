148c19d992d3fa1bfd3805549d2fd07f4174a401
Darel <johndarel0914@gmail.com>
2026-04-21 08:05:47 +0800

    Dynamically import condition icons and create toggle feature between Celcius and Farenheit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 
 import './style.css'
-const input = document.querySelector('input')
-const searchBtn = document.querySelector('.search-button')
-
-const key = 'YAGVLE6WETGXYAW8PQMHBUJAM'
-let isCelc = true 
 
 const select = (name) => {
     return document.querySelector(name)
 }
+
+const el = (type, options = {}) => {
+    const element = document.createElement(type)
+
+    if(options.class) element.className = options.class
+    if(options.src) element.src = options.src
+
+    return element
+}
+
+const input = select('#locInput')
+const searchBtn = select('.search-button')
+const toggle = select('.temp-toggle > button')
+
+const key = 'YAGVLE6WETGXYAW8PQMHBUJAM'
+let isCelc = true 
+let currentData = {}
 
 const getWeather = async (location) => {
     const req = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${key}`
@@ -34,39 +46,76 @@ const interpretData = (data) => {
 
 const interpretTemp = (num) => { 
     return isCelc 
-    ? Math.round((num - 32) * 5 / 9 * 10) / 10
+    ? Math.round(((num - 32) * 5 / 9) * 10) / 10
     : Math.round(num * 10) / 10
 }
 
-const displayWeather = (data) => {   
+const displayTemp = (data) => {
     const tempEl = select('.temp')
+    const feelsEl = select('.feels-like')
+
+    tempEl.textContent = `${interpretTemp(data.temp)}°`
+    feelsEl.textContent = `Feels like ${interpretTemp(data.feelslike)}°`
+}
+
+const displayWeather = async(data) => {   
+    console.log(data)
+
     const conditionEl = select('.condition')
     const locationEl = select('.location')
-    const feelsEl = select('.feels-like')
-    console.log(tempEl.textContent)
+    const iconElement = select('.condition-icon')
+    const icon = data.icon
+    const iconSrc = await import(`./asset/icons/${icon}.svg`)
 
     locationEl.textContent = data.location
-    tempEl.textContent = `${interpretTemp(data.temp)}°`
     conditionEl.textContent = data.condition
-    feelsEl.textContent = `Feels like ${interpretTemp(data.feelslike)}°`
+    displayTemp(data)
+    
+    iconElement.innerHTML = ''
+    const img = el('img', {src: iconSrc.default})
+    iconElement.appendChild(img) 
 }
 
 const search = async() => {
     if (input.value.length === 0) return
 
+    const searchVal = input.value.replace(/ /g, "-")
     try {
-        const rawData = await getWeather(input.value)
+        const rawData = await getWeather(searchVal)
         const data = interpretData(rawData)
+        currentData = data
         displayWeather(data)
     } catch (err) {
         console.log(err)
     }
 }
 
+const toggleTempEl = () => {
+    displayTemp(currentData)
+    toggle.classList.toggle('celc', isCelc)
+}
+
 console.clear()
+
+input.value = 'dublin'
+search()
+toggleTempEl()
 
 searchBtn.addEventListener('click', search)
 
 input.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') search()
+})
+
+toggle.addEventListener('click', () => {
+    isCelc = !isCelc
+    toggleTempEl()
+})
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === '/' && document.activeElement !== input) {
+        e.preventDefault()
+        input.focus()
+    }
+
 })
